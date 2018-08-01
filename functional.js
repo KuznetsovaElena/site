8d0e04f51c99b2327cbb73634b7471fd9191fd03
Your Name <you@example.com>
2018-08-01 22:31:55 +0300

    add new style

--- functional.js
+++ functional.js
@@ -4,13 +4,13 @@
  * and open the template in the editor.
  */
 
-$( function() {$( "#tabs" ).tabs({
+$( function() {$( ".tabs" ).tabs({
     collapsible: true,
     heightStyle: "fill"
     });
 });
 
-$( function() {$( "#accordion" ).accordion({
+$( function() {$( ".accordion" ).accordion({
     activeIndex: false,
     collapsible: true,
     heightStyle: "content"
@@ -18,9 +18,11 @@ $( function() {$( "#accordion" ).accordion({
 });
 
 var elem=null;
-dataHeight=$(window).height()-90;
+//dataHeight=$(window).height()-90;
+
 function changeData(type, path){
-    var contentData = document.getElementById("contentData");
+//    var contentData = document.getElementById("contentData");
+    var contentData = document.getElementById("content");
     if(elem!==null)
     {
         contentData.removeChild(elem);
@@ -34,7 +36,9 @@ function changeData(type, path){
         elem=document.createElement('iframe');
         elem.setAttribute('src',"https://docs.google.com/viewer?url="+path+"&embedded=true");
         elem.setAttribute('width','100%');
-        elem.setAttribute('height',dataHeight);
+        elem.setAttribute('frameBorder','0');
+        elem.setAttribute('class', 'ui-widget-shadow');
+//        elem.setAttribute('height',dataHeight);
         contentData.appendChild(elem);
     }
     else if(type==='html')
@@ -42,12 +46,15 @@ function changeData(type, path){
         elem=document.createElement('iframe');
         elem.setAttribute('src',path);
         elem.setAttribute('width','100%');
-        elem.setAttribute('height',dataHeight);
+        elem.setAttribute('frameBorder','0');
+        elem.setAttribute('class', 'ui-widget-shadow');
+//        elem.setAttribute('height',dataHeight);
         contentData.appendChild(elem);
     }
     else if(type==='flash')
     {
         elem=document.createElement('object');
+        elem.setAttribute('class', 'ui-widget-shadow');
         elem.setAttribute('type', 'application/x-shockwave-flash');
         elem.param('move').value=path;
         elem.param('allowScriptAccess').value='sameDomain';
@@ -57,6 +64,9 @@ function changeData(type, path){
 };
 
 window.onresize = function(event) {
+    var header = document.getElementById("header");
+    var footer = document.getElementById("footer");
+    
     dataHeight=$(window).height()-68;
     if(dataPath!==null){
         var data=document.getElementById('objectData');

5b6ca2a2c4d5ed985ec9df08d48ed9fc55c32170
Your Name <you@example.com>
2018-08-19 15:17:01 +0300

    fix resize

--- functional.js
+++ functional.js
@@ -34,41 +34,102 @@ function changeData(type, path){
             
             elem=document.createElement('iframe');
             elem.setAttribute('src',"https://docs.google.com/viewer?url="+path+"&embedded=true");
-            elem.setAttribute('width','100%');
-            elem.setAttribute('frameBorder','0');
-            elem.setAttribute('class', 'ui-widget-shadow');
-            elem.setAttribute('height','100%');
             contentData.appendChild(elem);
+            elem.setAttribute('class', 'ui-widget-shadow');
+            elem.setAttribute('width','100%');
+            elem.setAttribute('height','100%');
+            elem.setAttribute('frameBorder','0');
         }
         else if(type==='iframe')
         {
             elem=document.createElement('iframe');
             elem.setAttribute('src',path);
-            elem.setAttribute('width','100%');
-            elem.setAttribute('frameBorder','0');
-            elem.setAttribute('class', 'ui-widget-shadow');
-            elem.setAttribute('height','100%');
             contentData.appendChild(elem);
-//            elem.height='100%';
-//            elem.contentWindow.document.body.scrollHeight;
+            elem.setAttribute('class', 'ui-widget-shadow');
+            elem.setAttribute('width','100%');
+            elem.setAttribute('height','100%');
+            elem.setAttribute('frameBorder','0');
+            
         }
         else if(type==='swf')
         {
             elem=document.createElement('embed');
             elem.setAttribute('pluginspage','http://www.macromedia.com/go/getflashplayer');
             elem.setAttribute('src',path);
-            elem.setAttribute('width','100%');
-            elem.setAttribute('frameBorder','0');
-            elem.setAttribute('class', 'ui-widget-shadow');
-            elem.setAttribute('height','100%');
             contentData.appendChild(elem);
+            elem.setAttribute('class', 'ui-widget-shadow');
+            elem.setAttribute('width','100%');
+            elem.setAttribute('height','100%');
+            elem.setAttribute('frameBorder','0');
         }
+//        else if(type==='html')
+//        {
+//            if ('.' === path.substring(0,1)){
+//                var href = window.location.href;
+//                var dir = href.substring(0, href.lastIndexOf('/'));
+//                path=dir+path.substring(1);
+//            }
+//            
+//            // 1. Создаём новый объект XMLHttpRequest
+//            var xhr = new XMLHttpRequest();
+//
+//            // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
+//            xhr.open('GET', path, false);
+//
+//            // 3. Отсылаем запрос
+//            xhr.send();
+//
+//            // 4. Если код ответа сервера не 200, то это ошибка
+//            if (xhr.status !== 200) {
+//              // обработать ошибку
+//              alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+//            } else {
+//              // вывести результат
+//              
+//              innerHTML=xhr.responseText;
+//              
+////              var start = innerHTML.indexOf("<body");
+////              var end = -1;
+////              if(start !== -1)
+////              {
+////                  start = innerHTML.indexOf(">", start);
+////                  if(start !== -1)
+////                  {
+////                      end = innerHTML.lastIndexOf("</body>");
+////                      if(end === -1)
+////                      {
+////                          end = innerHTML.lastIndexOf("</html>");
+////                      }
+////                      if(end === -1)
+////                      {
+////                          end = innerHTML.length;    // If no HTML then just grab everything till end
+////                      }
+////                  }
+////              }
+//              alert(innerHTML);
+////              if(start !== -1 && end !==-1)
+////              {
+////                  innerHTML=innerHTML.slice(start + 1, end);
+////              }
+////              else
+////              {
+////                  innerHTML="Содержимое не загружено";
+////              }
+//              elem=document.createElement('div');
+//              elem.setAttribute('width','100%');
+//              elem.setAttribute('frameBorder','0');
+//              elem.setAttribute('class', 'ui-widget-shadow');
+//              elem.innerHTML = innerHTML;
+//              contentData.appendChild(elem);
+//            }
+//        }
     }
 };
 
-jQuery( window ).on( "orientationchange", function( event ) {
-    if(elem!==null)
-    {
-        document.getElementById("content").reload();
-    }
-});
+//jQuery( window ).on( "orientationchange", function( event ) {
+//    if(elem!==null)
+//    {
+//        document.getElementById("content").reload();
+//        document.getElementById("content").setAttribute('style','background: gray;');
+//    }
+//});
